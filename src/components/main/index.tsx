65b944d20d1a571794db3278f8a122918d77a92d
Patrick Garcia <patrickvicentegarcia@gmail.com>
2022-12-12 23:55:06 -0300

    style: updated style customizations

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,12 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avancado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Atom image with Advanced React description." />
+    <S.Title>Advanced React</S.Title>
+    <S.Description  >Typescript, ReactJS, NextJS and Styled Components</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="Developer coding on your computer"></S.Illustration>
+  </S.Wrapper>
 )
 
 export default Main
